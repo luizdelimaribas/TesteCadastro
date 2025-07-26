0a831ef57120c9f7173239bc0c270f4bc4fcde58
Luizdelimaribas <luizdelimaribas@live.com>
2025-07-26 19:25:38 -0300

    first commit

---
+++ src/main/java/com/ProjetoDemo/TesteCadastro/TesteCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetoDemo.TesteCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCadastroApplication.class, args);
+	}
+
+}
